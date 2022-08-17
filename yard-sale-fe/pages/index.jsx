cb2c46c684c8255092ed4c41373d603cfc400ac6
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-08-17 11:39:16 -0700

    get basic content from Sanity

--- yard-sale-fe/pages/index.jsx
+++ yard-sale-fe/pages/index.jsx
@@ -1,10 +1,43 @@
-// index.js
-const Index = () => {
+import client from "../client";
+
+const Index = ({ listings }) => {
+  console.log(listings);
   return (
     <div>
       <h1>Hello world!</h1>
+      <ul>
+        {listings.length > 0 &&
+          listings.map((listing, i) => {
+            return (
+              <li key={i}>
+                <h3>{listing.title}</h3>
+                {listing.categories.length > 0 &&
+                  listing.categories.map((category, i) => {
+                    return <span key={i}>{category.title}</span>;
+                  })}
+              </li>
+            );
+          })}
+      </ul>
     </div>
   );
 };
 
+export async function getStaticProps() {
+  const listings = await client.fetch(
+    `*[_type == 'product']{
+      title,
+      categories[] -> {
+        title,
+        'slug':slug.current
+},
+    'slug':slug.current,
+    }`
+  );
+  return {
+    props: {
+      listings,
+    },
+  };
+}
 export default Index;
