79ac8f042c7329bcad917c093eb335002686a0b9
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-08-23 12:29:35 -0700

    instruction to get all paths, link to listing tile

--- yard-sale-fe/pages/product/[slug].jsx
+++ yard-sale-fe/pages/product/[slug].jsx
@@ -1,9 +1,9 @@
-// post.js
+import client from "../../client";
 import { useRouter } from "next/router";
 
-const Post = () => {
+const Listing = ({ listing }) => {
+  console.log(listing);
   const router = useRouter();
-
   return (
     <article>
       <h1>{router.query.slug}</h1>
@@ -11,4 +11,34 @@ const Post = () => {
   );
 };
 
-export default Post;
+export async function getStaticPaths() {
+  const paths = await client.fetch(
+    `*[_type == "product" && defined(slug.current)][].slug.current`
+  );
+  return {
+    paths: paths.map((slug) => ({ params: { slug } })),
+    fallback: false,
+  };
+}
+
+// query to filter out what to return
+const listingQuery = `*[_type == "product"]{
+  title,
+  'slug': slug.current,
+  price, 
+  'blub': blub.en,
+  'image': images[0].asset -> url,
+  location,
+}`;
+
+// get data from CMS for listing
+export async function getStaticProps() {
+  const listing = await client.fetch(listingQuery);
+  return {
+    props: {
+      listing,
+    },
+  };
+}
+
+export default Listing;
