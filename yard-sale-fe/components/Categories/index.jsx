0b44444aa71f2b55acdd1795f3957af117b064bd
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-08-29 10:07:49 -0700

    category button links to new page

--- yard-sale-fe/components/Categories/index.jsx
+++ yard-sale-fe/components/Categories/index.jsx
@@ -4,21 +4,23 @@ import styles from "./styles.module.scss";
 
 const Categories = ({ data }) => {
   const router = useRouter();
-  // user selection updates the query in the url
+  // // user selection updates the query in the url
   const handleChange = (e) => {
-    if (router.isReady && router.asPath.includes(e.target.value)) {
-      //TODO: if category is already selected and selected again, category is untoggled by making it undefined. Can we remove category entirely?
-      router.push({
-        pathname: router.pathname,
-        query: { ...router.query, category: undefined },
-      });
-    } else {
-      router.push({
-        pathname: router.pathname,
-        query: { ...router.query, category: e.target.value },
-      });
-    }
+    // if (router.isReady && router.asPath.includes(e.target.value)) {
+    //   //TODO: if category is already selected and selected again, category is untoggled by making it undefined. Can we remove category entirely?
+    //   router.push({
+    //     pathname: router.pathname,
+    //     query: { ...router.query, category: undefined },
+    //   });
+    // } else {
+    //   router.push({
+    //     pathname: router.pathname,
+    //     query: { ...router.query, category: e.target.value },
+    //   });
+    // }
+    router.push(`/category/${e.target.value}`);
   };
+
   return (
     <div>
       <h2>All categories</h2>
@@ -30,11 +32,7 @@ const Categories = ({ data }) => {
                 <button
                   onClick={handleChange}
                   value={category.slug.current}
-                  className={clsx(
-                    styles.categoryBtn,
-                    category.slug.current === router.query.category &&
-                      styles.active
-                  )}
+                  className={clsx(styles.categoryBtn)}
                 >
                   {category.title}
                 </button>
