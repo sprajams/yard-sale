f4be5fa5e95f36e49c195e85c28d069be7f65ba3
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-09-01 16:19:44 -0700

    styling clean up

--- yard-sale-fe/pages/_app.js
+++ yard-sale-fe/pages/_app.js
@@ -1,6 +1,11 @@
 import { Component } from "react";
 import "../styles/global.css";
+import MainLayout from "../layout/MainLayout";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <MainLayout>
+      <Component {...pageProps} />
+    </MainLayout>
+  );
 }
