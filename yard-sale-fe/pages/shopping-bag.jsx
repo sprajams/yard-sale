3834575abb72d4ae3f6d385f57383f52ab8bbf02
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-09-09 08:43:03 -0700

    fetch server side props from cookie data

--- yard-sale-fe/pages/shopping-bag.jsx
+++ yard-sale-fe/pages/shopping-bag.jsx
@@ -1,14 +1,19 @@
 import client from "../client";
 import ShoppingBag from "../layout/Cart";
 
-export async function getServerSideProps() {
-  const bag = await client.fetch(
-    `*[_type == 'product'] {
+export async function getServerSideProps(context) {
+  const cookiesArr = JSON.parse(context.req.cookies.cartItems);
+  const cookiesIDs = cookiesArr.map((item) => item.id) || "";
+
+  const cookieQuery = `*[_id in $ids] {
     'id': _id,
     title,
+    'image': images[0].asset -> url,
     price,
-  }`
-  );
+  }`;
+
+  const bag = await client.fetch(cookieQuery, { ids: cookiesIDs });
+
   return {
     props: {
       bag,
